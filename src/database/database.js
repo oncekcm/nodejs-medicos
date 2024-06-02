aca5926354d5e84269a1a84dd78cdfa5355d24c3
oncekcm <59295554+oncekcm@users.noreply.github.com>
2024-06-02 15:54:32 -0400

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -5,7 +5,8 @@ export const sequelize = new Sequelize(
   "projectsdb_sfhi_user", // username
   "qiJ9NG9bPVtIURwUknt5lAeI6liQObNt", // password
   {
-    host: "postgres://projectsdb_sfhi_user:qiJ9NG9bPVtIURwUknt5lAeI6liQObNt@dpg-cpec6avsc6pc739dhr2g-a.oregon-postgres.render.com/projectsdb_sfhi",
+    host: "dpg-cpec6avsc6pc739dhr2g-a.oregon-postgres.render.com",
+    port: "5432",
     dialect: "postgres",
     // pool: {
     //   max: 5,
